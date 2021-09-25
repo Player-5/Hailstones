b41d75d6b7c9df64501bf86e9369e39178bd3aa2
Player-5 <57650225+Player-5@users.noreply.github.com>
2021-09-25 11:49:12 -0400

    Added Licence terms to source

--- hailstone.py
+++ hailstone.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+#    Main script for Hailstones 
+#    Copyright (C) 2021  Player-5
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+ #   You should have received a copy of the GNU Affero General Public License
+ #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 A python3 module used to calculate hailstone Numbers, testing if they terminate
 into 4, 2, 1. and recording relevent data about the proccese.
